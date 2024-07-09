f67507f1a0ae6923660b5f029375a86c7e7f4fc8
Kirill Bannyi <kbannyi@custis.ru>
2024-07-09 14:22:38 +0300

    feat: добавил флаги из переменных окружения

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 type Flags struct {
@@ -15,6 +16,13 @@ func ParseConfig() Flags {
 	flag.StringVar(&flags.RunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&flags.RedirectBaseAddr, "b", "http://localhost:8080/", "base for short links")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
+		flags.RunAddr = envRunAddr
+	}
+	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
+		flags.RedirectBaseAddr = envBaseAddr
+	}
 
 	fmt.Println("Running on:", flags.RunAddr)
 	fmt.Println("Base for short links:", flags.RedirectBaseAddr)
