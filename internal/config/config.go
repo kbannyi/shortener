aecd1ed271c7225684cffff38d761b6f79751422
Kirill Bannyi <kbannyi@custis.ru>
2024-07-29 20:14:34 +0300

    feat: добавил флаг FileStoragePath

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,12 +8,14 @@ import (
 type Flags struct {
 	RunAddr          string
 	RedirectBaseAddr string
+	FileStoragePath  string
 }
 
 func ParseConfig() Flags {
 	flags := Flags{}
 	flag.StringVar(&flags.RunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&flags.RedirectBaseAddr, "b", "http://localhost:8080/", "base for short links")
+	flag.StringVar(&flags.FileStoragePath, "f", "index.json", "path to file storage")
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
@@ -21,6 +23,9 @@ func ParseConfig() Flags {
 	}
 	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
 		flags.RedirectBaseAddr = envBaseAddr
+	}
+	if envBaseAddr := os.Getenv("FILE_STORAGE_PATH"); envBaseAddr != "" {
+		flags.FileStoragePath = envBaseAddr
 	}
 
 	return flags
