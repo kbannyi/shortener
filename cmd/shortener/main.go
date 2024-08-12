31d74291817eccb0db1f6a442a8f91b3b7b871cb
Kirill Bannyi <kbannyi@custis.ru>
2024-08-12 20:18:32 +0300

    feat: ping для базы данных

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/kbannyi/shortener/internal/config"
 	"github.com/kbannyi/shortener/internal/logger"
 	"github.com/kbannyi/shortener/internal/middleware"
@@ -21,6 +25,17 @@ func main() {
 		return
 	}
 
+	var db *sql.DB
+	var err error
+	if flags.DatabaseURI != "" {
+		db, err = sql.Open("pgx", flags.DatabaseURI)
+		if err != nil {
+			logger.Log.Errorf("Unable to connect to database: %v\n", err)
+			return
+		}
+		defer db.Close()
+	}
+
 	repo, err := repository.NewRepository(flags)
 	if err != nil {
 		logger.Log.Errorf("Coudn't initialize repository: %v", err)
@@ -31,6 +46,7 @@ func main() {
 	r.Use(middleware.RequestLoggerMiddleware)
 	r.Use(middleware.ResponseLoggerMiddleware)
 	r.Use(middleware.GZIPMiddleware)
+	r.Get("/ping", ping(db))
 	r.Mount("/", router.NewURLRouter(serv, flags))
 
 	logger.Log.Info("Starting server...")
@@ -39,5 +55,21 @@ func main() {
 	logger.Log.Infow("Using storage file:", "path", flags.FileStoragePath)
 	if http.ListenAndServe(flags.RunAddr, r) != nil {
 		logger.Log.Errorf("Error on serve: %v", err)
+	}
+}
+
+func ping(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if db == nil {
+			http.Error(w, "failed to connect to db", http.StatusInternalServerError)
+			return
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+		if err := db.PingContext(ctx); err != nil {
+			http.Error(w, "failed to connect to db", http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
